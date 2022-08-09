1a9d3b6496ef16781c27ab1e183710e1289da953
Fabri <fabriciogrotta@gmail.com>
2022-08-09 18:31:25 -0300

    JS terminado, HTML terminado. falta CSS

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ const tasksContainer = document.getElementById("tasksContainer")
 
 const setDate = () => {
     const date = new Date();
-    dateNumber.textContent = date.toLocaleString(es, {day: "numeric"});
-    dateText.textContent = date.toLocaleString(es, {weekday: "long"});
-    dateMonth.textContent = date.toLocaleString(es, {month: "short"});
-    dateYear.textContent = date.toLocaleString(es, {year: "numeric"});
+    dateNumber.textContent = date.toLocaleString("es", {day: "numeric"});
+    dateText.textContent = date.toLocaleString("es", {weekday: "long"});
+    dateMonth.textContent = date.toLocaleString("es", {month: "short"});
+    dateYear.textContent = date.toLocaleString("es", {year: "numeric"});
 };
 
 const addNewTask = e => {
@@ -25,11 +25,38 @@ const addNewTask = e => {
     task.addEventListener("click", changeTaskState)
     task.textContent = value;
     tasksContainer.prepend(task);
+    e.target.reset();
 };
 
 const changeTaskState = e => {
     e.target.classList.toggle("done");
-
 };
+
+// const order = () => {
+//     const done = [];
+//     const toDo = [];
+//     tasksContainer.childNodes.forEach(elemento => {
+//         elemento.classList.contains("done") ? done.push(elemento) : toDo.push(elemento)
+//     })
+//     return [...toDo, ...done];
+// }
+
+// const renderOrderedTasks = () => {
+//     order().forEach(elemento => tasksContainer.appendChild(elemento))
+// }
+
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
 
 setDate();
